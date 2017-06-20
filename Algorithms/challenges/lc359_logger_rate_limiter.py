3c8f816b56162e5be14a19d7dc5e0b05310da65c
Ning Liu <ning@liu.onl>
2017-06-20 09:22:26 -0700

    Add lc359.py.

---
+++ Algorithms/challenges/lc359_logger_rate_limiter.py
@@ -0,0 +1,58 @@
+"""
+Time: O(1), amortized for rehashing
+Space: O(k), k is the max number of printed messages in last 10 seconds.
+
+Design a logger system that receive stream of messages along with its
+timestamps, each message should be printed if and only if it is not printed in
+the last 10 seconds.
+
+Given a message and a timestamp (in seconds granularity), return true
+if the message should be printed in the given timestamp, otherwise
+returns false.
+
+It is possible that several messages arrive roughly at the same time.
+
+Example:
+
+Logger logger = new Logger();
+
+// logging string "foo" at timestamp 1
+logger.shouldPrintMessage(1, "foo"); returns true;
+
+// logging string "bar" at timestamp 2
+logger.shouldPrintMessage(2,"bar"); returns true;
+
+// logging string "foo" at timestamp 3
+logger.shouldPrintMessage(3,"foo"); returns false;
+
+// logging string "bar" at timestamp 8
+logger.shouldPrintMessage(8,"bar"); returns false;
+
+// logging string "foo" at timestamp 10
+logger.shouldPrintMessage(10,"foo"); returns false;
+
+// logging string "foo" at timestamp 11
+logger.shouldPrintMessage(11,"foo"); returns true;
+"""
+
+
+class Logger(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self._printed = {}
+
+    def shouldPrintMessage(self, timestamp, message):
+        """
+        Returns true if the message should be printed in the given timestamp,
+        otherwise returns false. The timestamp is in seconds granularity.
+        :type timestamp: int
+        :type message: str
+        :rtype: bool
+        """
+        if timestamp < self._printed.get(message, 0) + 10:
+            return False
+        self._printed[message] = timestamp
+        return True

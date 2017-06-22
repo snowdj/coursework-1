b092377b3ef60e7f8299113b1ac3771b5808fd35
Ning Liu <ning@liu.onl>
2017-06-22 12:11:34 -0700

    Add lc226.py.

---
+++ Algorithms/challenges/lc226_invert_binary_tree.py
@@ -0,0 +1,58 @@
+"""
+Time: O(n)
+Space: O(n) for recursion stacks.
+
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+Google: 90% of our engineers use the software you wrote (Homebrew),
+but you can’t invert a binary tree on a whiteboard so fuck off.
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+# BFS walk
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root:
+            root.left, root.right = root.right, root.left
+            self.invertTree(root.left)
+            self.invertTree(root.right)
+            return root
+
+
+# DFS walk
+class Solution2(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root:
+            root.left, root.right = self.invertTree(root.right),\
+                                    self.invertTree(root.left)
+            return root

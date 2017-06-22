48c548f2a09b43c732a905beb30d25d70205c0b0
Ning Liu <ning@liu.onl>
2017-06-22 13:20:27 -0700

    Add lc258.py.

---
+++ Algorithms/challenges/lc258_add_digits.py
@@ -0,0 +1,39 @@
+"""
+Time: O(1)
+Space: O(1)
+
+Given a non-negative integer num, repeatedly add all its digits until the
+result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only
+one digit, return it.
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+"""
+
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while(num >= 10):
+            temp = 0
+            while(num > 0):
+                temp += num % 10
+                num //= 10
+            num = temp
+        return num
+
+
+class Solution2(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        return (num-1) % 9 + 1 if num else 0

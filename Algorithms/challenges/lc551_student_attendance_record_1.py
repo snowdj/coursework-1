f322034dd7556582581e5f5bf647a90abc48884f
Jesse Liu <jesse@liu.onl>
2017-06-30 16:37:30 -0700

    Add lc551.py.

---
+++ Algorithms/challenges/lc551_student_attendance_record_1.py
@@ -0,0 +1,34 @@
+"""
+Time: O(n)
+Space: O(1)
+
+You are given a string representing an attendance record for a student. The
+record only contains the following three characters:
+
+'A' : Absent.
+'L' : Late.
+'P' : Present.
+A student could be rewarded if his attendance record doesn't contain more than
+one 'A' (absent) or more than two continuous 'L' (late).
+
+You need to return whether the student could be rewarded according to his
+attendance record.
+
+Example 1:
+Input: "PPALLP"
+Output: True
+Example 2:
+Input: "PPALLL"
+Output: False
+"""
+
+
+import re
+
+
+def checkRecord(self, s):
+    return not re.search('A.*A|LLL', s)
+
+
+def checkRecord2(self, s):
+    return not (s.count('A') > 1 or 'LLL' in s)

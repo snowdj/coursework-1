801bf6d4320720973ffd8d152e474dba87ea6b45
Jesse Liu <jesse@liu.onl>
2017-06-30 18:16:00 -0700

    Add lc541.py.

---
+++ Algorithms/challenges/lc541_reverse_string_2.py
@@ -0,0 +1,43 @@
+"""
+Time: O(n)
+Space: O(1) iterative; O(n/2k) recursive.
+
+
+Given a string and an integer k, you need to reverse the first k characters for
+every 2k characters counting from the start of the string. If there are less
+than k characters left, reverse all of them. If there are less than 2k but
+greater than or equal to k characters, then reverse the first k characters and
+left the other as original.
+
+Example:
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+Restrictions:
+The string consists of lower English letters only.
+Length of the given string and k will in the range [1, 10000]
+"""
+
+
+# iterative
+class Solution(object):
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        s = list(s)
+        for i in range(0, len(s), 2*k):
+            s[i:i+k] = s[i:i+k][::-1]
+        return ''.join(s)
+
+
+# recursive
+class Solution2(object):
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        return s[:k][::-1] + s[k:2*k] + self.reverseStr(s[2*k:], k) if s else ""

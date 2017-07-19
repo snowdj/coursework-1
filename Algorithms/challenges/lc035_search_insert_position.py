801f9d1ac743b9b9a331872930ba22716a876a99
Jesse Liu <jesse@liu.onl>
2017-07-19 15:06:45 -0700

    Add lc035.py.

---
+++ Algorithms/challenges/lc035_search_insert_position.py
@@ -0,0 +1,33 @@
+"""
+Time: O(log(n))
+Space: O(1)
+
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+"""
+
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums)
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                right = mid
+        return right

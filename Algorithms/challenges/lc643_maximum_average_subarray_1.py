d5d7d7cd27dfa769a709676c52b4bd4f585b9b98
Jesse Liu <jesse@liu.onl>
2017-07-18 18:05:30 -0700

    Add lc643.py.

---
+++ Algorithms/challenges/lc643_maximum_average_subarray_1.py
@@ -0,0 +1,32 @@
+"""
+Time: O(n)
+Space: O(1)
+
+Given an array consisting of n integers, find the contiguous subarray of given
+length k that has the maximum average value. And you need to output the
+maximum average value.
+
+Example 1:
+Input: [1,12,-5,-6,50,3], k = 4
+Output: 12.75
+
+Explanation: Maximum average is (12-5-6+50)/4 = 51/4 = 12.75
+
+Note:
+1. 1 <= k <= n <= 30,000.
+2. Elements of the given array will be in the range [-10,000, 10,000].
+"""
+
+
+class Solution:
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        s = m = sum(nums[:k])
+        for i in range(len(nums)-k):
+            s = s - nums[i] + nums[i+k]
+            m = max(m, s)
+        return m / k

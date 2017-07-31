636f5c8d1013b0a4572fe28201ed92ebc3d1ff9f
Jesse Liu <jesse@liu.onl>
2017-07-30 21:25:57 -0700

    Add lc278.py.

---
+++ Algorithms/challenges/lc278_first_bad_version.py
@@ -0,0 +1,39 @@
+"""
+Time: O(lg(n))
+Space: O(1)
+
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+"""
+
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        lo, hi = 0, n
+        while lo < hi:
+            mid = lo + (hi-lo)//2
+            if isBadVersion(mid):
+                hi = mid
+            else:
+                lo = mid + 1
+        return lo
+
+
+# bisect
+def firstBadVersion(self, n):
+    class Wrap:
+        def __getitem__(self, i):
+            return isBadVersion(i)
+    return bisect.bisect(Wrap(), False, 0, n)

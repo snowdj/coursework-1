e1d92d9274e366062751f226fb827214aefedb19
Jesse Liu <jesse@liu.onl>
2017-08-31 11:42:26 -0700

    Stuck in the middle of chapter 4, page 57.

--- TDD_Python/superlists/lists/views.py
+++ TDD_Python/superlists/lists/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 # from django.http import HttpResponse
 
@@ -5,4 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     # return HttpResponse('<html><title>To-Do lists</title></html>')
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
+    return render(request, 'home.html',
+                  {'new_item_text': request.POST.get('item_text', '')})

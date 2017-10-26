d4d5954e23a910e268b210a7cbe6e90b65614d5f
Jesse Liu <jesse@liu.onl>
2017-10-25 23:27:37 -0700

    Add BFS and DFS.

--- Algorithms/graphs.py
+++ Algorithms/graphs.py
@@ -4,17 +4,45 @@ Algorithms on graphs.
 """
 
 
-def bfs(s, adj):
+def BFS(s, adj):
     """
     Code from MIT 6.006 Fall 2011, Lec 13 BFS.
     https://www.youtube.com/watch?v=s-CYnVz-uh4 34:10
+    Time: \theta(V+E)
+
+    :param s: starting vertex
+    :param adj: adjacency lists representing a graph
     """
-    pass
+    level = {s: 0}
+    parent = {s: None}
+    i = 1
+    frontier = [s]
+    while frontier:
+        next_ = []
+        for u in frontier:
+            for v in adj[u]:
+                if v not in level:
+                    level[v] = i
+                    parent[v] = u
+                    next_.append(v)
+        frontier = next_
+        i += 1
 
 
-def dfs():
+def DFS(V, adj):
     """
     Code from MIT 6.006 Fall 2011, Lec 14 DFS.
     https://www.youtube.com/watch?v=AfSk24UTFS8  3:50
+    Time: \theta(V+E)
     """
-    pass
+    def DFS_visit(adj, s):
+        for v in adj[s]:
+            if v not in parent:  # not visited before
+                parent[v] = s
+                DFS_visit(adj, v)
+
+    parent = {}
+    for s in V:
+        if s not in parent:
+            parent[s] = None
+            DFS_visit(adj, s)

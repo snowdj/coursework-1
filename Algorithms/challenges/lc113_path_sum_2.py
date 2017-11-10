de6b160dadda2c386404921f38bf5dbc39046371
Jesse Liu <jesse@liu.onl>
2017-11-10 13:33:18 -0800

    Add lc113.py

---
+++ Algorithms/challenges/lc113_path_sum_2.py
@@ -0,0 +1,46 @@
+"""
+Time: O(n)
+Space: O(lg(n))
+
+
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+For example:
+Given the below binary tree and sum = 22,
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+return
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        if not root.left and not root.right and root.val == sum:
+            return [[root.val]]
+        return [[root.val] + tail for tail in
+                self.pathSum(root.left, sum-root.val)
+                + self.pathSum(root.right, sum-root.val)]

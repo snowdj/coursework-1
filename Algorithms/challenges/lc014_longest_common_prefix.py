625943e231dfa2563d9be67b4857a70614296e74
Ning Liu <4315932+ningliu0@users.noreply.github.com>
2018-05-11 22:28:32 -0700

    Add lc014.cc

--- Algorithms/challenges/lc014_longest_common_prefix.py
+++ Algorithms/challenges/lc014_longest_common_prefix.py
@@ -3,6 +3,22 @@ Time: O(n)
 Space: O(1), excluding return.
 
 Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+
+Input: ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+Note:
+
+All given inputs are in lowercase letters a-z.
 """
 
 
